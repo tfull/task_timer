e6365a313e70f838381ce7074be57be9a555dc10
tfull <tfull154008@gmail.com>
2014-12-01 22:49:52 +0900

    run.bat

--- run.bat
+++ run.bat
@@ -8,12 +8,12 @@ java -cp .;bin pack.Timers
 exit /b
 
 :week
-for /f %%t in ('date /t') do set nt=%%t
-if %nt% == "0" (
-java -cp .;bin pack.Timers "data/weekend.txt"
-) else if %nt% == "6" (
-java -cp .;bin pack.Timers "data/weekend.txt"
+for /f %%t in ('java -cp bin pack.Week') do set nt=%%t
+if "%nt%" == "0" (
+java -cp .;bin pack.Timers "data\weekend.txt"
+) else if "%nt%" == "6" (
+java -cp .;bin pack.Timers "data\weekend.txt"
 ) else (
-java -cp .;bin pack.Timers "data/title.txt"
+java -cp .;bin pack.Timers "data\title.txt"
 )
 exit /b
